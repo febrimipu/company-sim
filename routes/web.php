42d9909c308561734e01554dadaaaaa0ef3fb897
Nervt <macbook@Mr-Nervt.local>
2024-05-22 21:09:12 +0700

    fresh install + bootsrap auth

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Dashboard;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', [Dashboard::class, 'index'])->name('dashboard');
